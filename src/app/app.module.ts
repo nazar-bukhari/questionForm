f944e8b57a3adb9d4a5a0d2a64aa98a1f8916a41
nazar-bukahri <ratul.pentagon@gmail.com>
2017-12-23 07:32:46 +0600

    Front end post api part done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { BcsformComponent } from './bcsform/bcsform.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/bcsform/bcsform.component.ts
+++ src/app/bcsform/bcsform.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Bcs } from '../bcs';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-bcsform',
@@ -7,6 +8,8 @@ import { Bcs } from '../bcs';
   styleUrls: ['./bcsform.component.css']
 })
 export class BcsformComponent {
+
+  constructor(private http: HttpClient) {}
 
    bcsQuestion = new Bcs('Who did what?', 'op1', 'op2', 'op3', 'op4', 2 , 'NaN');
    submitted = false;
@@ -17,6 +20,22 @@ export class BcsformComponent {
      return JSON.stringify(this.bcsQuestion);
    }
    newQuestion() {
-     this.bcsQuestion = new Bcs('New Question?', '', '', '', '', 2 , '');
+     // this.bcsQuestion = new Bcs('New Question?', '', '', '', '', 2 , '');
+     console.log(JSON.stringify(this.bcsQuestion));
+
+     this.http
+       .post('/questions', JSON.stringify(this.bcsQuestion))
+       // See below - subscribe() is still necessary when using post().
+       .subscribe();
    }
+
+
+  // ngOnInit(): void {
+  //   // Make the HTTP request:
+  //   this.http
+  //     .post('/questions', JSON.stringify(this.bcsQuestion))
+  //     // See below - subscribe() is still necessary when using post().
+  //     .subscribe();
+  // }
 }
+
